173dafd9a8a5b2dba1ed5ca548bb7926db2ea4c2
Joshua Cheung <contact.cjoshua@gmail.com>
2022-05-12 16:07:08 -0700

    refactor the code and create callback function in index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,25 @@
 const request = require("request");
-const breed = process.argv.slice(2);
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  function (error, response, body) {
-    const data = JSON.parse(body);
-    if (typeof data[0] == "undefined") {
-      const error = new Error("Not Found");
-      console.log("error:", error); // ''not found
-    } else {
-      console.log(data[0].description);
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    function (error, response, body) {
+      const data = JSON.parse(body);
+
+      let description = null;
+      if (typeof data[0] == "undefined") {
+        error = new Error("Not Found");
+        // console.log("error:", error); // ''not found
+      } else if (response.statusCode != 200) {
+        error = new Error("Connection Fail");
+        // console.log("error:", error);
+        // console.log("response:", response.statusCode);
+      } else {
+        description = data[0].description;
+      }
+      callback(error, description);
     }
-    if (response.statusCode != 200) {
-      const error = new Error("Connection Fail");
-      console.log("error:", error);
-      console.log("response:", response.statusCode);
-    }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
